f3d4da31b9a096e8a5b8e3cd24e2667cb307e53d
sangltv <sangltv@proton.me>
2022-10-28 13:30:51 +0000

    prisma

--- src/index.js
+++ src/index.js
@@ -1,7 +1,32 @@
 import http from "http"
 import express from "express"
 import dotenv from 'dotenv'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  // ... you will write your Prisma Client queries here
+  const usersWithPosts = await prisma.user.findMany({
+    include: {
+      posts: true,
+    },
+  })
+  console.dir(usersWithPosts, { depth: null })
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
+
 dotenv.config()
+
 
 const PORT = process.env.PORT || 3000
 
