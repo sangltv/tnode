b998e333932195b47fcfa55520b0ef692e358c5f
sangltv <sangltv@proton.me>
2022-11-04 11:30:13 +0000

    as

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,11 @@
+require("dotenv").config()
 const express = require("express")
 const app = express()
+const PORT = process.env.PORT
+
 app.get("/", (req,res)=>{
     res.send("TEST")
 })
 
-app.listen(3000,()=>{});
+
+app.listen(PORT,()=>{});
